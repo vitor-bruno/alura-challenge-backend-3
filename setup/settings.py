1741dc4b5903d87a7b4ae62981cb02b66d7b2f3f
Vitor Bruno <vbruno0110@gmail.com>
2022-05-27 18:43:26 -0300

    Preparação para deploy no Heroku

--- setup/settings.py
+++ setup/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alurafinancas',
         'USER': 'postgres',
-        'PASSWORD': 'vitinho0110',
+        'PASSWORD': config("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
